87c416af12bb933f86b2ef56bc8dce8bc96483a4
Devon Selvaggi <76073510+noclipping@users.noreply.github.com>
2022-11-17 22:05:19 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import "./app.css";
+import Form1 from "./Form1";
+import Form2 from "./Form2";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>localStorage with React hooks</h1>
+      {/* <Form1 /> */}
+      <Form2 />
+    </div>
+  );
+};
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+/*
+ This project used code from this article:
+ https://blog.logrocket.com/using-localstorage-react-hooks/
+ Thanks.
+*/
